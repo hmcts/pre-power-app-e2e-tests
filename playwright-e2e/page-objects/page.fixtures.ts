5de6b8b2d4768221911c6ec7077fa14f1184ea0d
Ali Hussain <Ali.Hussain@justice.gov.uk>
2025-07-11 15:27:06 +0100

    Restructured the project to allow for cvp and pre portal pages to be added later during e2e tests

---
+++ playwright-e2e/page-objects/page.fixtures.ts
@@ -0,0 +1,7 @@
+import { PowerAppPageFixtures, powerAppPageFixtures } from './power-app/power-app-page.fixtures.js';
+
+export type PageFixtures = PowerAppPageFixtures;
+
+export const pageFixtures = {
+  ...powerAppPageFixtures,
+};
