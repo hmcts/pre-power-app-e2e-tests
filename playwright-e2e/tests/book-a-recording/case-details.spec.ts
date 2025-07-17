6bd3e575cea4534aba41457531d5cb0837d22f34
Ali Hussain <Ali.Hussain@justice.gov.uk>
2025-07-17 10:04:04 +0100

    updated test description

--- playwright-e2e/tests/book-a-recording/case-details.spec.ts
+++ playwright-e2e/tests/book-a-recording/case-details.spec.ts
@@ -335,25 +335,25 @@ test.describe('Set of tests to verify case details page', () => {
   );
 
   test(
-    'Verify when accessing the caseDetailsPage all the three buttons are stated correctly',
+    'Verify when accessing the caseDetailsPage all the three buttons are in the correct state',
 
     {
       tag: '@Regression',
     },
 
     async ({ caseDetailsPage }) => {
-      (await test.step('Verify all the buttons are on casedetailspage should be visible', async () => {
+      await test.step('Verify all the buttons on case details page are visible', async () => {
         await expect(caseDetailsPage.$interactive.modifyButton).toBeVisible();
         await expect(caseDetailsPage.$interactive.saveButton).toBeVisible();
         await expect(caseDetailsPage.$interactive.bookingsButton).toBeVisible();
-      }),
-        await test.step('Verify modify and bookings buttons are disabled when accessing a casedetails page', async () => {
-          await expect(caseDetailsPage.$interactive.modifyButton).toBeDisabled();
-          await expect(caseDetailsPage.$interactive.bookingsButton).toBeDisabled();
-        }),
-        await test.step('Verify save button is Enabled when accessing a casedetails page', async () => {
-          await expect(caseDetailsPage.$interactive.saveButton).toBeEnabled();
-        }));
+      });
+      await test.step('Verify modify and bookings buttons are disabled when accessing case details page', async () => {
+        await expect(caseDetailsPage.$interactive.modifyButton).toBeDisabled();
+        await expect(caseDetailsPage.$interactive.bookingsButton).toBeDisabled();
+      });
+      await test.step('Verify save button is enabled when accessing case details page', async () => {
+        await expect(caseDetailsPage.$interactive.saveButton).toBeEnabled();
+      });
     },
   );
 });
