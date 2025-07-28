349e6f0e481f9a9a0c293e06abee5e70b8571198
Komal <97675424+komaldabhi1509@users.noreply.github.com>
2025-07-28 12:25:38 +0100

    Added new test to verify the buttons are in correc t state when exisiting case selected

--- playwright-e2e/tests/state-management-tests/book-a-recording/case-details-state.spec.ts
+++ playwright-e2e/tests/state-management-tests/book-a-recording/case-details-state.spec.ts
@@ -28,4 +28,32 @@ test.describe('Set of tests to verify buttons on the case details page are in th
       });
     },
   );
+  test(
+    'Verify when selecting an exisiting case, four buttons are in the correct state',
+    {
+      tag: '@Regression',
+    },
+    async ({ caseDetailsPage, apiClient }) => {
+      await test.step('Pre-requisite step in order to create a case via api', async () => {
+        await apiClient.createCase(2, 2);
+      });
+      const caseData = await apiClient.getCaseData();
+      await caseDetailsPage.searchAndSelectExistingCase(caseData.caseReference);
+
+      await test.step('Verify all four buttons are visible', async () => {
+        await expect(caseDetailsPage.$interactive.searchedCaseCloseButton).toBeVisible();
+        await expect(caseDetailsPage.$interactive.modifyButton).toBeVisible();
+        await expect(caseDetailsPage.$interactive.bookingsButton).toBeVisible();
+        await expect(caseDetailsPage.$interactive.saveButton).toBeVisible();
+      });
+      await test.step('Verify three buttons are enabled', async () => {
+        await expect(caseDetailsPage.$interactive.searchedCaseCloseButton).toBeEnabled();
+        await expect(caseDetailsPage.$interactive.modifyButton).toBeEnabled();
+        await expect(caseDetailsPage.$interactive.bookingsButton).toBeEnabled();
+      });
+      await test.step('Verify save button is disabled', async () => {
+        await expect(caseDetailsPage.$interactive.saveButton).toBeDisabled();
+      });
+    },
+  );
 });
