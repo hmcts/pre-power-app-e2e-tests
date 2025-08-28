d6d01ca8396ed7314cde2c24b66974be382a06f1
Ali Hussain <Ali.Hussain@justice.gov.uk>
2025-08-28 13:04:04 +0100

    Updated playwright config and pipeline yml file

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   testDir: './playwright-e2e',
   snapshotDir: './playwright-e2e/snapshots',
   ...CommonConfig.recommended,
+  reporter: [['list'], ['html', { outputFolder: 'playwright-report' }]],
   timeout: 120_000,
   projects: [
     {
