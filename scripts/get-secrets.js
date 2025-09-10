9d60ddf99a3c6f53de9bf2015e0be20478327d0a
Ali Hussain <Ali.Hussain@justice.gov.uk>
2025-09-10 12:17:55 +0100

    Updated scripts to fetch secrets

--- scripts/get-secrets.js
+++
@@ -1,6 +0,0 @@
-import { populateSecrets } from '@hmcts/playwright-common';
-// pre-hmctskv-test used for common secrets across all environments
-// pre-hmctskv-dev used for dev specific secrets, Update as needed
-const vaultNames = ['pre-hmctskv-test', 'pre-hmctskv-dev'];
-populateSecrets(vaultNames);
-
